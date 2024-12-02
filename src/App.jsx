8ecbe2704b9bb5ec8673864ff4c7f084b10eb726
Jakhongirbey <q.jakhongirbey@gmail.com>
2024-12-03 00:44:00 +0500

    my bab

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-
-import Header from "./components/Header";
 import Products from "./components/Products";
-import Swiperrr from "./components/Swiperr";
+import Modal from "./components/Modal";
+import { useState } from "react";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
+  const [modalType, setModalType] = useState(null);
+
+  const toggleActions = () => {
+    setModalType((prev) => (prev === "signIn" ? "signUp" : "signIn"));
+  };
+
   return (
     <>
-      <Header/>
-      <Swiperrr/>
+      <div className="flex justify-center space-x-4 my-4">
+        <button
+          onClick={() => setModalType("signIn")}
+          className="bg-blue-500 text-white py-2 px-4 rounded"
+        >
+          Sign in
+        </button>
+        <button
+          onClick={() => setModalType("signUp")}
+          className="bg-green-500 text-white py-2 px-4 rounded"
+        >
+          Sign up
+        </button>
+      </div>
+
+      {modalType === "signIn" && (
+        <Modal close={() => setModalType(null)}>
+          <SignIn toggle={toggleActions} />
+        </Modal>
+      )}
+
+      {modalType === "signUp" && (
+        <Modal close={() => setModalType(null)}>
+          <SignUp toggle={toggleActions} />
+        </Modal>
+      )}
       <Products />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { MdOutlineDarkMode } from "react-icons/md";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
 
 const Header = () => {
-  const darkMode = () => {
-    document.body.classList.toggle("dark");
-  };
+  
   return (
     <div className="w-[80%] mx-auto mt-4 ">
       <nav className="bg-[#F0ECE4] border-gray-200 dark:bg-gray-900">
@@ -46,55 +45,9 @@ const Header = () => {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <button onClick={darkMode}>
-                  <MdOutlineDarkMode className="text-white size-7" />
-                </button>
-              </li>
-            </ul>
+          <div className="hidden w-full md:block md:w-auto " id="navbar-default">
+            <SignIn />
+            <SignUp />
           </div>
         </div>
       </nav>
